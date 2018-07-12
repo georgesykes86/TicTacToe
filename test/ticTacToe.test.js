95a87fb7322f6cb9e53df8895de312864f0816a6
George Sykes <georgesykes86@gmail.com>
2018-07-12 17:39:08 +0100

    Refactor TicTacToe tests to incorporate Player mocking

--- test/ticTacToe.test.js
+++ test/ticTacToe.test.js
@@ -1,33 +1,51 @@
 const TicTacToe = require('../lib/ticTacToe');
+const Player = require('../lib/player');
+
+jest.mock('../lib/player');
 
 describe('TicTacToe', () => {
-  let game;
+  let game; let p1Spy; let p2Spy; let p1SelectionSpy; let p2SelectionSpy;
+  const player1 = new Player();
+  const player2 = new Player();
+  const firstMove = 'A1';
 
   beforeEach(() => {
-    game = new TicTacToe();
-    game.play('A1');
+    game = new TicTacToe(player1, player2);
+    p1Spy = jest.spyOn(player1, 'addMove');
+    p2Spy = jest.spyOn(player2, 'addMove');
+    p1Spy.mockClear();
+    p2Spy.mockClear();
+    game.play(firstMove);
   });
 
   describe('#play', () => {
-    it('registers a turn for player 1', () => {
-      expect(game.players.player1.moves.length).toEqual(1);
-    });
+    describe('No duplicate selections', () => {
+      beforeEach(() => {
+        p1SelectionSpy = jest.spyOn(player1, 'isAlreadySelectedMove').mockReturnValue(false);
+        p1SelectionSpy = jest.spyOn(player2, 'isAlreadySelectedMove').mockReturnValue(false);
+      });
 
-    it('registers a turn for player 2', () => {
-      game.play('B1');
-      expect(game.players.player2.moves.length).toEqual(1);
-    });
+      it('registers a turn for player 1', () => {
+        expect(p1Spy).toHaveBeenCalledWith(firstMove);
+      });
 
-    it('continues to switch goes', () => {
-      game.play('B1');
-      game.play('C1');
-      expect(game.players.player1.moves.length).toEqual(2);
+      it('registers a turn for player 2', () => {
+        let secondMove = 'B1';
+        game.play(secondMove);
+        expect(p2Spy).toHaveBeenCalledWith(secondMove);
+      });
+
+      it('continues to switch goes', () => {
+        game.play('B1');
+        game.play('C1');
+        expect(p1Spy).toHaveBeenCalledTimes(2);
+      });
     });
 
     it('stops a player selecting a square that has been taken', () => {
+      p1SelectionSpy = jest.spyOn(player1, 'isAlreadySelectedMove').mockReturnValue(true);
       game.play('A1');
-      expect(game.players.player2.moves.length).toEqual(0);
-      expect(game.turn).toEqual('player2');
+      expect(p1Spy).not.toHaveBeenCalledWith();
     });
   });
 });
