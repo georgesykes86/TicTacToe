4db2b9b8d02d17219b6af948b24007773165240c
George Sykes <georgesykes86@gmail.com>
2018-07-13 09:09:06 +0100

    Add showBoard to TicTacToe class

--- test/ticTacToe.test.js
+++ test/ticTacToe.test.js
@@ -4,13 +4,16 @@ const Player = require('../lib/player');
 jest.mock('../lib/player');
 
 describe('TicTacToe', () => {
-  let game; let p1Spy; let p2Spy;
+  let game; let p1Spy; let p2Spy; let printerSpy;
   const player1 = new Player();
   const player2 = new Player();
   const firstMove = 'A1';
+  const gamePrinter = {
+    printGame: jest.fn(),
+  };
 
   beforeEach(() => {
-    game = new TicTacToe(player1, player2);
+    game = new TicTacToe(player1, player2, gamePrinter);
     p1Spy = jest.spyOn(player1, 'addMove');
     p2Spy = jest.spyOn(player2, 'addMove');
     p1Spy.mockClear();
@@ -52,6 +55,14 @@ describe('TicTacToe', () => {
       game.isOver = true;
       game.play('B1');
       expect(p2Spy).not.toHaveBeenCalledWith();
+    });
+  });
+
+  describe('#showBoard', () => {
+    it('calls on the gamePrinter to show the board', () => {
+      printerSpy = jest.spyOn(gamePrinter, 'printGame');
+      game.showBoard();
+      expect(printerSpy).toHaveBeenCalledWith(player1, player2);
     });
   });
 });
